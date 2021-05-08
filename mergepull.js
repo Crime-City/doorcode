25f420f378aff50f0689ef71026a1f494eb58f60
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-05-08 09:10:04 +0100

    Update mergepull.js

--- mergepull.js
+++ mergepull.js
@@ -1,6 +1,9 @@
 if(event.button=0){
   $this.frameIndex(0)
 }
-if(event.button=1){
+if(event.button=2){
   $this.frameIndex(1)
+}
+if(event.button=1){
+  window.alert("left click to close, right click to open");
 }
